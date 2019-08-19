1bfb05be42be832b262b4c4c87d007280976bf94
wilson <wilson@Mac-mini-de-wilson.local>
2019-08-19 11:42:07 -0300

    feature: GlobalStyle added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
   env: {
-    browser: true,
     es6: true,
   },
   extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -18,7 +19,12 @@ module.exports = {
   plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
-    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
     'import/prefer-default-export': 'off',
   },
 };
